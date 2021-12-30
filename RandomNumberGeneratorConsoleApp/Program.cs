e06638fd72567605350464015e9a67c1181e59e0
JoDaUT <jose.david.urena.torres@gmail.com>
2021-12-29 22:20:02 -0600

    Linear Congruential Generator implementation

---
+++ RandomNumberGeneratorConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using PRNG;
+namespace PRNG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+       
+            LCG rnd = new();
+            double[] array = rnd.RandomArray(size: 20, min: 1, max: 20);
+            foreach (double n in array)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
