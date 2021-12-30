353d2afcf7ef4d9b2ee9ab946c1560e6dfdafeda
JoDaUT <jose.david.urena.torres@gmail.com>
2021-12-29 22:59:34 -0600

    Removed unnecesary code and comments

--- RandomNumberGeneratorConsoleApp/Program.cs
+++ RandomNumberGeneratorConsoleApp/Program.cs
@@ -7,15 +7,11 @@ namespace PRNG
         static void Main(string[] args)
         {
        
-            LCG rnd = new(seed:0,multiplier:1,increment:1,modulus:0);
-            //double[] array = rnd.RandomArray(size: 20, min: 1, max: 20);
-            //foreach (double n in array)
-            //{
-            //    Console.WriteLine(n);
-            //}
-            for (int i = 0; i < 30; i++)
+            LCG rnd = new();
+            double[] array = rnd.RandomArray(size: 20, min: 1, max: 20);
+            foreach (double n in array)
             {
-                Console.WriteLine(rnd.NextNumber());
+                Console.WriteLine(n);
             }
 
         }
