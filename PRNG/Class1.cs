1cf15f6e57368e6800be5a374d2eb367869f6d70
JoDaUT <jose.david.urena.torres@gmail.com>
2021-12-29 22:22:01 -0600

    LCG class added

--- PRNG/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace PRNG
-{
-    public class Class1
-    {
-    }
-}
---
+++ PRNG/LCG.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace PRNG
+{
+    //Linear Congruential Generator implementation
+    public class LCG
+    {
+        double seed;
+        private readonly double multiplier;
+        private readonly double increment;
+        private readonly double modulus;
+
+        public LCG()
+        {
+            this.seed = DateTime.Now.Ticks;
+            this.multiplier = 83939;
+            this.increment = 30911;
+            this.modulus = int.MaxValue;
+        }
+        public LCG(double seed, double multiplier, double increment, double modulus)
+        {
+            this.seed = seed;
+            this.multiplier = multiplier;
+            this.increment = increment;
+            this.modulus = modulus;
+        }
+        public double NextNumber()
+        {
+            double number = (this.multiplier * this.seed + this.increment) % this.modulus;
+
+            this.seed = number;
+            return number;
+        }
+        public double NextNumber(int min, int max)
+        {
+            return this.NextNumber() % (max-min+1) + min;
+        }
+        public double[] RandomArray(int size)
+        {
+            double[] array = new double[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = this.NextNumber();
+            }
+            return array;
+        }
+        public double[] RandomArray(int size, int min, int max)
+        {
+            double[] array = new double[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = this.NextNumber(min, max);
+            }
+            return array;
+        }
+
+    }
+}
